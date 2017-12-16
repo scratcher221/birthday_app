a8643bc747a259a6ba78ab2e7878ec2193ad7609
David Mitterlehner <david@davids-mbp.home>
2017-12-16 14:34:30 +0100

    Initial Commit

---
+++ birthday_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  birthday_app
+//
+//  Created by David Mitterlehner on 16.12.17.
+//  Copyright © 2017 David und Bernadette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
